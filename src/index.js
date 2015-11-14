47c72bfd330f014f3d2270679d47bb115060103b
Georges <georges.plancke@simplifield.com>
2015-11-14 20:01:45 +0100

    [Major update] reaccess now need "METHOD /url/toTest" as input

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ angular.module('simplifield.reaccess', ['ng'])
   var currentValues = [];
   var globalDebug = false;
 
+  var cache = {};
+
   this.setPredefinedRights = function(value) {
     predefinedRights = value;
   };
@@ -21,125 +23,103 @@ angular.module('simplifield.reaccess', ['ng'])
   this.$get = ['$log', function SFReaccessServiceFactory($log) {
     var sfReaccessService = {
       setRights: function sfReaccessServiceSetRights(rights) {
+        cache = {} // Empty cache;
         currentRights = rights;
       },
       setValues: function sfReaccessServiceSetValues(values) {
         currentValues = values;
       },
       _debugging: globalDebug,
-      match: function sfReaccessServiceMatch(predefinedRight, templateValues, debug) {
+      test: function sfReaccessServiceMatch(url, debug) {
         var right;
         var path;
         var rightPath;
         try {
-          rights = predefinedRights[predefinedRight] instanceof Array ?
-            predefinedRights[predefinedRight] :
-            [predefinedRights[predefinedRight]];
-          return rights.every(function(right) {
-            if((!right) || 'undefined' == typeof right.path
-              || 'undefined' == typeof right.methods
-              || !right.methods.length) {
+          var parsedUrl = url.split(' ');
+          var methods = parsedUrl[0].split(',');
+          var rightPath = parsedUrl[1];
+          var isMatching = false;
+
+          if('undefined' !== typeof cache[url]) {
+            if(debug) {
+              $log.debug('sfReaccess: Serving right resolution from cache for' + url);
+            }
+            return cache[url];
+          }
+
+          if(currentRights.some(function(currentRight, i) {
+            var result = false;
+            if(!methods.every(function(method) {
+              return -1 !== currentRight.methods.indexOf(method);
+            })) {
               if(debug) {
-                $log.error('sfReaccess: ' + predefinedRight + ': No or invalid right' +
-                  ' object (requiring path, methods properties).');
+                $log.debug('sfReaccess: ' + url + ' : ' + i + ': Methods' +
+                  ' doesn\'t match.', methods, currentRight.methods);
               }
               return false;
             }
-            rightPath = right.path;
-            while(/(.*\/|^):([a-z0-9_\-\.\*\@\#]+)(\/.*|$)/.test(rightPath)) {
-              rightPath = rightPath.replace(/(.*\/|^):([a-z0-9_\-\.\*\@\#]+)(\/.*|$)/,
-                function($, $1, $2, $3) {
-                  var values = getValues(templateValues.concat(currentValues), $2);
-                  if(debug) {
-                    $log.debug('sfReaccess: ' + predefinedRight + ': Found the' +
-                      ' templated value "' + $2 + '" in the predefined right,' +
-                      ' resolved it to the following values:', values, templateValues.concat(currentValues));
+            path = currentRight.path;
+            if(currentValues) {
+              while(/(.*\/|^):([a-z0-9_\-\.\*\@\#]+)(\/.*|$)/.test(path)) {
+                path = path.replace(
+                  /(.*\/|^):([a-z0-9_\-\.\*\@\#]+)(\/.*|$)/,
+                  function($, $1, $2, $3) {
+                    var values = getValues(currentValues, $2);
+                    if(debug) {
+                      $log.debug('sfReaccess: ' + url + ' : ' + i +
+                        ' Found the templated value "' + $2 + '" in the current' +
+                        ' tested right, resolved it to the following values:', values, currentValues);
+                    }
+                    if(values.length) {
+                      return $1 + (1 === values.length ?
+                        escRegExp(values[0]) :
+                        '(' + values.map(escRegExp).join('|') + ')') + $3;
+                    }
+                    return '';
                   }
-                  if(values.length) {
-                    return $1 + values[0] + $3;
-                  }
-                  return '';
-                });
+                );
+              }
             }
-            if(currentRights.some(function(currentRight, i) {
-              var result = false;
-              if(!right.methods.every(function(method) {
-                return -1 !== currentRight.methods.indexOf(method);
-              })) {
-                if(debug) {
-                  $log.debug('sfReaccess: ' + predefinedRight + ': ' + i + ': Methods' +
-                    ' doesn\'t match.', right.methods, currentRight.methods);
-                }
-                return false;
-              }
-              path = currentRight.path;
-              if(currentValues) {
-                while(/(.*\/|^):([a-z0-9_\-\.\*\@\#]+)(\/.*|$)/.test(path)) {
-                  path = path.replace(/(.*\/|^):([a-z0-9_\-\.\*\@\#]+)(\/.*|$)/,
-                    function($, $1, $2, $3) {
-                      var values = getValues(currentValues, $2);
-                      if(debug) {
-                        $log.debug('sfReaccess: ' + predefinedRight + ': ' + i +
-                          ' Found the templated value "' + $2 + '" in the current' +
-                          ' tested right, resolved it to the following values:', values, currentValues);
-                      }
-                      if(values.length) {
-                        return $1 + (1 === values.length ?
-                          escRegExp(values[0]) :
-                          '(' + values.map(escRegExp).join('|') + ')') + $3;
-                      }
-                      return '';
-                    });
-                }
-              }
-              result = path && new RegExp('^'+path+'$').test(rightPath)
-              if(debug) {
-                $log.debug('sfReaccess: ' + predefinedRight + ': ' + i + ': Testing "' +
-                  rightPath + '" against "/^' + path.replace('/', '\\/') + '$/"' +
-                  ' (original path "' + currentRight.path + '")' +
-                  ' led to ', result ? 'SUCCESS' : 'FAILURE');
-              }
-              return result;
-            })) {
-              return true;
+            result = path && new RegExp('^'+path+'$').test(rightPath)
+            if(debug) {
+              $log.debug('sfReaccess: ' + url + ' : ' + i + ': Testing "' +
+                rightPath + '" against "/^' + path.replace('/', '\\/') + '$/"' +
+                ' (original path "' + currentRight.path + '")' +
+                ' led to ', result ? 'SUCCESS' : 'FAILURE');
             }
-            return null;
-          }) ? rightPath : null;
+            return result;
+          })) {
+            isMatching = true;
+          }
+
+          cache[url] = isMatching;
+          return isMatching;
+
         } catch(err) {
           if(debug) {
-            $log.debug('sfReaccess: ' + predefinedRight + ': FAILURE due "' +
+            $log.debug('sfReaccess: ' + url + ': FAILURE due "' +
               'to the following error:', err);
           }
           return null;
         }
       },
-      test: function sfReaccessServiceTest() {
-        var res = sfReaccessService.match.apply(sfReaccessService, arguments);
-
-        return (res !== null);
-      }
     };
 
     return sfReaccessService;
   }];
 }).filter('sfReaccess', ['$log', 'sfReaccessService',
   function ($log, sfReaccessService) {
-  return function(predefinedRights, templateValues, debug) {
-    debug = 'boolean' === typeof debug ? debug : sfReaccessService._debugging;
-    predefinedRights = predefinedRights || [];
-    templateValues = (templateValues instanceof Array ? templateValues :
-      (templateValues ? [templateValues] : []));
-    if(!(predefinedRights instanceof Array)) {
-      predefinedRights = [predefinedRights];
-    }
-    if(debug) {
-      $log.debug('sfReaccess: New filter execution:', predefinedRights, templateValues);
-    }
-    return predefinedRights.every(function(predefinedRight) {
-      return sfReaccessService.test(predefinedRight, templateValues, debug);
-    });
-  };
-}]);
+    return function(url, debug) {
+      debug = 'boolean' === typeof debug ? debug : sfReaccessService._debugging;
+
+      if(debug) {
+        $log.debug('sfReaccess: New filter execution:', url, templateValues);
+      }
+
+      return sfReaccessService.test(url, debug);
+    };
+  }
+]);
 
 // Helpers
 function getValues(values, path) {
